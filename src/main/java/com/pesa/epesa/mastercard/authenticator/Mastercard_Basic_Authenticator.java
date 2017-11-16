e1545a3472c1889456c5d1bf917ad13828991de8
johnnjoroge <kiragujohn@hotmail.com>
2017-11-16 19:03:35 +0200

    cert

--- src/main/java/com/pesa/epesa/mastercard/authenticator/Mastercard_Basic_Authenticator.java
+++ src/main/java/com/pesa/epesa/mastercard/authenticator/Mastercard_Basic_Authenticator.java
@@ -9,9 +9,11 @@ import com.mastercard.api.core.ApiConfig;
 import com.mastercard.api.core.security.oauth.OAuthAuthentication;
 import com.pesa.epesa.mastercard.resources.Mastercard_Properties;
 import com.pesa.epesa.mastercard.resources.Property;
+import com.pesa.epesa.mastercard.resources.mastercard_Client;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -19,11 +21,13 @@ import java.io.InputStream;
  */
 public class Mastercard_Basic_Authenticator {
     public static void basic_Authenticate() throws FileNotFoundException{
+      final Logger logger = Logger.getLogger(mastercard_Client.class);  
       String consumerKey = Mastercard_Properties.getProperty(Property.CONSUMERKEY);
       String keyAlias =  Mastercard_Properties.getProperty(Property.KEYALIAS);            
       String keyPassword = Mastercard_Properties.getProperty(Property.KEYPASSWORD);
       //InputStream is = new FileInputStream(Mastercard_Properties.getProperty(Property.KEYSTOREPATH));  
-      InputStream is = new FileInputStream(Mastercard_Properties.getProperty(Property.KEYSTOREPATH));  
+      InputStream is = new FileInputStream(Mastercard_Properties.getProperty(Property.KEYSTOREPATH));
+        logger.info("File was cound at.............."+Mastercard_Properties.getProperty(Property.KEYSTOREPATH));
       ApiConfig.setAuthentication(new OAuthAuthentication(consumerKey, is, keyAlias, keyPassword)); 
       ApiConfig.setDebug(true);   
       ApiConfig.setSandbox(true);
